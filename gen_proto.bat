5fdce078f7203024a99040c8d7b38ed22ff4e3f4
MalitsPlus <hua8102259@gmail.com>
2022-11-23 00:47:57 +0900

    feat: update protos

--- gen_proto.bat
+++ gen_proto.bat
@@ -1,7 +1,14 @@
+@echo off
+
 python proto_analyzer_meta.py
 python proto_analyzer_ts.py
 protoc -I=D:/GitHub/SolisClient/cache --python_out=D:/GitHub/SolisClient/cache ProtoEnum.proto ProtoMaster.proto ProtoTransaction.proto
+protoc -I=D:/GitHub/SolisClient/cache --python_out=D:/GitHub/SolisClient/cache ProtoApi.proto 
 :: add services
+
+echo === please add sevices and continue ===
+pause
+
 protoc -I=D:/GitHub/SolisClient/cache --python_out=D:/GitHub/SolisClient/cache ProtoApi.proto 
 python -m grpc_tools.protoc --proto_path=./cache ./cache/ProtoApi.proto --grpc_python_out=./cache
 python gen_grpc_aftermath.py
