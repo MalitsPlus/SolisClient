96cacf5060e59195adeeb26796c0504298134ee5
MalitsPlus <50281195+MalitsPlus@users.noreply.github.com>
2025-04-18 20:33:55 +0900

    refactor: remove service addition prompt from gen_proto.bat

--- gen_proto.bat
+++ gen_proto.bat
@@ -4,10 +4,6 @@ python proto_analyzer_meta.py
 python proto_analyzer_ts.py
 protoc -I=D:/GitHub/SolisClient/cache --python_out=D:/GitHub/SolisClient/cache ProtoEnum.proto ProtoMaster.proto ProtoTransaction.proto
 protoc -I=D:/GitHub/SolisClient/cache --python_out=D:/GitHub/SolisClient/cache ProtoApi.proto 
-:: add services
-
-echo === please add sevices and continue ===
-pause
 
 protoc -I=D:/GitHub/SolisClient/cache --python_out=D:/GitHub/SolisClient/cache ProtoApi.proto 
 python -m grpc_tools.protoc --proto_path=./cache ./cache/ProtoApi.proto --grpc_python_out=./cache
